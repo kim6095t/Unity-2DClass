4c5e2434b9bc2c483e5d0c3a3473c16e08199762
Seungmok <mol1986@naver.com>
2021-08-10 15:53:55 +0900

    Step 19 - 2D Platform

--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -36,14 +36,12 @@ public class TitleManager : MonoBehaviour
         if(DataManager.IsSavedData)
         {
             // 세이브 파일 존재. 삭제할 것인지 물어본다.
-            Debug.Log("진짜 지웁니까?");
-
             PopupManager.Instance.ShowPopup(
                 "새 게임 시작",
                 "기존의 데이터를 삭제합니다.\n정말 삭제하시겠습니까?",
-                "그래요", "싫어요",
+                new PopupManager.ButtonHandle("아니오", "예"),
                 (index) => {  
-                    if(index == 0)
+                    if(index == 1)
                         OnNewGameAsDelete();
                 });
         }
@@ -55,7 +53,12 @@ public class TitleManager : MonoBehaviour
     }
     public void Continue()
     {
-        SceneMover.Instance.MoveScene("WorldMap");
+        PopupManager.Instance.ShowPopup("이어하기", "게임을 이어합니다.",
+            new PopupManager.ButtonHandle("GO!"),
+            (index) => {
+
+                SceneMover.Instance.MoveScene("WorldMap");
+            });
     }
     public void OpenOption()
     {
@@ -63,6 +66,11 @@ public class TitleManager : MonoBehaviour
     }
     public void ExitGame()
     {
-        Debug.Log("ExitGame");
+        PopupManager.Instance.ShowPopup("게임 종료", "게임을 종료하시겠습니까?",
+            new PopupManager.ButtonHandle("예", "네", "끌게요"),
+            (index) => {
+
+                Debug.Log("선택된 버튼은 : " + index);
+            });
     }
 }
--- Assets/Scripts/UI/PopupManager.cs
+++ Assets/Scripts/UI/PopupManager.cs
@@ -5,11 +5,45 @@ using UnityEngine.UI;
 
 public class PopupManager : Singletone<PopupManager>
 {
+    public enum BUTTON_TYPE
+    {
+        Confirm,        // 확인. 버튼 1개.
+        Choice,         // 선택. 버튼 2개.
+        Triple,         // 3가지.
+    }
+
+    public struct ButtonHandle
+    {
+        public string[] buttonNames;
+        public BUTTON_TYPE type;
+
+        public ButtonHandle(string one, string two, string three)
+        {
+            buttonNames = new string[3];
+            buttonNames[0] = one;
+            buttonNames[1] = two;
+            buttonNames[2] = three;
+            type = BUTTON_TYPE.Triple;
+        }
+
+        public ButtonHandle(string one, string two)
+            : this(one, two, string.Empty)
+        {
+            type = BUTTON_TYPE.Choice;
+        }
+
+        public ButtonHandle(string one)
+            : this(one, string.Empty, string.Empty)
+        {
+            type = BUTTON_TYPE.Confirm;
+        }
+    }
+
+
     [SerializeField] Transform panel;       // 패널.
     [SerializeField] Text titleText;        // 타이틀 텍스트.
     [SerializeField] Text context;          // 내용 텍스트.
-    [SerializeField] Button leftButton;     // 왼쪽 버튼.
-    [SerializeField] Button rightButton;    // 오른쪽 버튼.
+    [SerializeField] Button[] buttons;      // 버튼 배열.
 
     System.Action<int> OnCallback;          // 콜백.
 
@@ -29,12 +63,19 @@ public class PopupManager : Singletone<PopupManager>
         OnCallback(index);
         SwitchPopup(false);
     }
-    public void ShowPopup(string title, string context, string left, string right, System.Action<int> OnCallback)
+
+
+    public void ShowPopup(string title, string context, ButtonHandle handle, System.Action<int> OnCallback)
     {
         titleText.text = title;
         this.context.text = context;
-        leftButton.GetComponentInChildren<Text>().text = left;
-        rightButton.GetComponentInChildren<Text>().text = right;                
+
+        int buttonCount = (int)handle.type;       // 전달 받은 버튼 개수.
+        for(int i = 0; i<buttons.Length; i++)     // 전체 버튼 배열을 순회.
+        {
+            buttons[i].gameObject.SetActive(i <= buttonCount);          // 버튼을 끄고 켠다.
+            buttons[i].gameObject.GetComponentInChildren<Text>().text = handle.buttonNames[i];      // 버튼의 텍스트 대입.
+        }
 
         this.OnCallback = OnCallback;
         SwitchPopup(true);
